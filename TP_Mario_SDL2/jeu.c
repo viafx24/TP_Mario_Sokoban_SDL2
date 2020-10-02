a39a5b655371ef18c94c266c2480b035f1dd9077
Guillaume Baptist <guillaume.baptist@free.fr>
2020-10-02 19:19:54 +0200

    removing useless comment

--- TP_Mario_SDL2/jeu.c
+++ TP_Mario_SDL2/jeu.c
@@ -15,6 +15,9 @@ void jouer(SDL_Window* win, SDL_Renderer* rend)
 	SDL_Rect position, positionJoueur;
 	SDL_Event event;
 
+	position.w = 1 * TAILLE_BLOC;
+	position.h = 1 * TAILLE_BLOC;
+
 	int continuer = 1, objectifsRestants = 0, i = 0, j = 0;
 	int carte[NB_BLOCS_LARGEUR][NB_BLOCS_HAUTEUR] = { 0 };
 
@@ -107,29 +110,10 @@ void jouer(SDL_Window* win, SDL_Renderer* rend)
 		}
 
 		// Effacement de l'écran
-
-		position.w = 1 * TAILLE_BLOC;
-		position.h = 1 * TAILLE_BLOC;
-
-		//position.x = 1;
-		//position.y = 1;
-
-		//SDL_QueryTexture(murTex, NULL, NULL, &position.w, &position.h);
-
-
 		SDL_SetRenderDrawColor(rend, 255, 255, 255, 255);
 		SDL_RenderClear(rend);
-		//SDL_RenderCopy(rend, murTex, NULL, &position);
 
-		//
-		//SDL_RenderPresent(rend);
-
-	
-
-
-		/*	SDL_SetRenderDrawColor(rend, 255, 255, 255, 255);
-
-		}*/	        // Placement des objets à l'écran
+// Placement des objets à l'écran
 		objectifsRestants = 0;
 
 		for (i = 0; i < NB_BLOCS_LARGEUR; i++)
@@ -144,22 +128,21 @@ void jouer(SDL_Window* win, SDL_Renderer* rend)
 				case MUR:
 					SDL_QueryTexture(murTex, NULL, NULL, &position.w, &position.h);
 					SDL_RenderCopy(rend, murTex, NULL, &position);
-					//SDL_BlitSurface(mur, NULL, ecran, &position);
+	
 					break;
 				case CAISSE:
 					SDL_QueryTexture(caisseTex, NULL, NULL, &position.w, &position.h);
 					SDL_RenderCopy(rend, caisseTex, NULL, &position);
-					//SDL_BlitSurface(caisse, NULL, ecran, &position);
+
 					break;
 				case CAISSE_OK:
 					SDL_QueryTexture(caisseOKTex, NULL, NULL, &position.w, &position.h);
 					SDL_RenderCopy(rend, caisseOKTex, NULL, &position);
-					//SDL_BlitSurface(caisseOK, NULL, ecran, &position);
+
 					break;
 				case OBJECTIF:
 					SDL_QueryTexture(objectifTex, NULL, NULL, &position.w, &position.h);
 					SDL_RenderCopy(rend, objectifTex, NULL, &position);
-					//SDL_BlitSurface(objectif, NULL, ecran, &position);
 					objectifsRestants = 1;
 					break;
 				}
@@ -176,261 +159,12 @@ void jouer(SDL_Window* win, SDL_Renderer* rend)
 
 		SDL_QueryTexture(marioActuel, NULL, NULL, &position.w, &position.h);
 		SDL_RenderCopy(rend, marioActuel, NULL, &position);
-		//SDL_BlitSurface(marioActuel, NULL, ecran, &position);
-
-
-//		SDL_RenderClear(rend);
 		SDL_RenderPresent(rend);
-		//   SDL_Flip(ecran);
+
 	}
 
 }
-	
-//	    // Désactivation de la répétition des touches (remise à 0)
-//	    //SDL_EnableKeyRepeat(0, 0);
-//	
-//	    // Libération des surfaces chargées
-//	    SDL_FreeSurface(mur);
-//	    SDL_FreeSurface(caisse);
-//	    SDL_FreeSurface(caisseOK);
-//	    SDL_FreeSurface(objectif);
-//	    for (i = 0 ; i < 4 ; i++)
-//	        SDL_FreeSurface(mario[i]);
-//	}
-//	printf("test");
-////	SDL_EnableKeyRepeat(100, 100);
-//}
-    // Activation de la répétition des touches
- 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-	//position.w = 1 * TAILLE_BLOC;
-	//position.h = 1 * TAILLE_BLOC;
-
-	//position.x = 1;
-	//position.y = 1;
-
-	//SDL_QueryTexture(murTex, NULL, NULL, &position.w, &position.h);
-
- //   SDL_SetRenderDrawColor(rend, 255, 255, 255, 255);
-
-	//SDL_RenderClear(rend);
-	//SDL_RenderCopy(rend, murTex, NULL, &position);
-	//
-	//SDL_RenderPresent(rend);
-
-
-
-	//SDL_SetRenderDrawColor(rend, 0, 0, 0, 255);
-	//SDL_Rect rectangle;
-
-	//rectangle.x = 0;
-	//rectangle.y = 0;
-	//rectangle.w = 50;
-	//rectangle.h = 50;
-	//SDL_RenderFillRect(rend, &rectangle);
-
-	//SDL_RenderPresent(rend);
-
-
-
-	//SDL_RenderClear(rend);
-	//SDL_SetRenderDrawColor(rend, 127, 127, 127, 255);
-
-
-	//SDL_RenderPresent(rend);
-
-
-//	SDL_Delay(2000); //Pause execution for 3000 milliseconds, for example
-//
-//	// destroy texture 
-//	//SDL_DestroyTexture(murTex);
-//	//SDL_DestroyTexture(caisseTex);
-//
-//	//// destroy renderer 
-//	//SDL_DestroyRenderer(rend);
-//
-//	//// destroy window 
-//	//SDL_DestroyWindow(win);
-//
-//
-//	return 0;
-//
-//}
-
-
-//	SDL_QueryTexture(murTex, NULL, NULL, &position.w, &position.h);
-//
-//	position.w = 1 * TAILLE_BLOC;
-//	position.h = 1 * TAILLE_BLOC;
-//
-//	SDL_QueryTexture(caisseTex, NULL, NULL, &position.w, &position.h);
-//
-//	positionJoueur.w = 2 * TAILLE_BLOC;
-//	positionJoueur.h = 1 * TAILLE_BLOC;
-//
-//
-//	SDL_RenderCopy(rend, murTex, NULL, &position);
-//	SDL_RenderCopy(rend, caisseTex, NULL, &positionJoueur);
-//	// triggers the double buffers 
-//	// for multiple rendering 
-//	SDL_RenderPresent(rend);
-//
-//
-//	SDL_Delay(10000);  // Pause execution for 3000 milliseconds, for example
-//
-//	// destroy texture 
-//	SDL_DestroyTexture(murTex);
-//	SDL_DestroyTexture(caisseTex);
-//
-//	// destroy renderer 
-//	SDL_DestroyRenderer(rend);
-//
-//	// destroy window 
-//	SDL_DestroyWindow(win);
-//
-//
-//	return 0;
-//
-//}
-
-//
-//
-//    marioActuel = mario[BAS]; // Mario sera dirigé vers le bas au départ
-//
-//    // Chargement du niveau
-//    if (!chargerNiveau(carte))
-//        exit(EXIT_FAILURE); // On arrête le jeu si on n'a pas pu charger le niveau
-//
-//    // Recherche de la position de Mario au départ
-//    for (i = 0 ; i < NB_BLOCS_LARGEUR ; i++)
-//    {
-//        for (j = 0 ; j < NB_BLOCS_HAUTEUR ; j++)
-//        {
-//            if (carte[i][j] == MARIO) // Si Mario se trouve à cette position sur la carte
-//            {
-//                positionJoueur.x = i;
-//                positionJoueur.y = j;
-//                carte[i][j] = VIDE;
-//            }
-//        }
-//    }
-//
-//    // Activation de la répétition des touches
-//    SDL_EnableKeyRepeat(100, 100);
-//
-//    while (continuer)
-//    {
-//        SDL_WaitEvent(&event);
-//switch(event.type)
-//{
-//    case SDL_QUIT:
-//        continuer = 0;
-//        break;
-//    case SDL_KEYDOWN:
-//        switch(event.key.keysym.sym)
-//        {
-//            case SDLK_ESCAPE:
-//                continuer = 0;
-//                break;
-//            case SDLK_UP:
-//                marioActuel = mario[HAUT];
-//                deplacerJoueur(carte, &positionJoueur, HAUT);
-//                break;
-//            case SDLK_DOWN:
-//                marioActuel = mario[BAS];
-//                deplacerJoueur(carte, &positionJoueur, BAS);
-//                break;
-//            case SDLK_RIGHT:
-//                marioActuel = mario[DROITE];
-//                deplacerJoueur(carte, &positionJoueur, DROITE);
-//                break;
-//            case SDLK_LEFT:
-//                marioActuel = mario[GAUCHE];
-//                deplacerJoueur(carte, &positionJoueur, GAUCHE);
-//                break;
-//        }
-//        break;
-//}
-//
-//        // Effacement de l'écran
-//        SDL_FillRect(ecran, NULL, SDL_MapRGB(ecran->format, 255, 255, 255));
-//
-//        // Placement des objets à l'écran
-//        objectifsRestants = 0;
-//
-//        for (i = 0 ; i < NB_BLOCS_LARGEUR ; i++)
-//        {
-//            for (j = 0 ; j < NB_BLOCS_HAUTEUR ; j++)
-//            {
-//                position.x = i * TAILLE_BLOC;
-//                position.y = j * TAILLE_BLOC;
-//
-//                switch(carte[i][j])
-//                {
-//                    case MUR:
-//                        SDL_BlitSurface(mur, NULL, ecran, &position);
-//                        break;
-//                    case CAISSE:
-//                        SDL_BlitSurface(caisse, NULL, ecran, &position);
-//                        break;
-//                    case CAISSE_OK:
-//                        SDL_BlitSurface(caisseOK, NULL, ecran, &position);
-//                        break;
-//                    case OBJECTIF:
-//                        SDL_BlitSurface(objectif, NULL, ecran, &position);
-//                        objectifsRestants = 1;
-//                        break;
-//                }
-//            }
-//        }
-//
-//        // Si on n'a trouvé aucun objectif sur la carte, c'est qu'on a gagné
-//        if (!objectifsRestants)
-//            continuer = 0;
-//
-//        // On place le joueur à la bonne position
-//        position.x = positionJoueur.x * TAILLE_BLOC;
-//        position.y = positionJoueur.y * TAILLE_BLOC;
-//        SDL_BlitSurface(marioActuel, NULL, ecran, &position);
-//
-//
-//
-//        SDL_Flip(ecran);
-//    }
-//
-//    // Désactivation de la répétition des touches (remise à 0)
-//    SDL_EnableKeyRepeat(0, 0);
-//
-//    // Libération des surfaces chargées
-//    SDL_FreeSurface(mur);
-//    SDL_FreeSurface(caisse);
-//    SDL_FreeSurface(caisseOK);
-//    SDL_FreeSurface(objectif);
-//    for (i = 0 ; i < 4 ; i++)
-//        SDL_FreeSurface(mario[i]);
-//}
-//
 void deplacerJoueur(int carte[][NB_BLOCS_HAUTEUR], SDL_Rect *pos, int direction)
 {
     switch(direction)
